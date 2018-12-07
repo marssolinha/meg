8903158dd3bbc9aa545516734cbcc1677f232f61
Mauro Marssola <mauro.marssola@visie.com.br>
2018-12-07 11:53:41 -0200

    Added support to defined extension type to OpenCart

--- modules/opencart.rb
+++ modules/opencart.rb
@@ -5,6 +5,7 @@ module Meg
     attr_accessor :pluginname
     $application_path = "#{__dir__}/.."
     $language = "en-gb"
+    $extension = "module"
 
     include Thor::Actions
     include MyCli::SharedMethods
@@ -20,6 +21,33 @@ module Meg
       File.dirname(__FILE__)
     end
 
+    method_option :plugintype, :type => :string, :aliases => "-t", :default => "admin-extension"
+    method_option :language, :type => :string, :aliases => "-l", :default => "en-gb"
+    method_option :extension, :type => :string, :aliases => "-e", :default => "module"
+    method_option :appdir, :type => :string, :aliases => "-a", :required => true
+    desc "plugin PLUGINNAME", "Generates a new plugin named PLUGINNAME"
+    
+    def plugin(pluginname, *fields)
+        self.pluginname = pluginname
+        plugintype = options[:plugintype]
+        appdir = options[:appdir]
+        $language = options[:language]
+        $extension = options[:extension]
+        
+        opts = { :pluginname => pluginname, :fields => fields }
+            
+        puts "Generating '#{plugintype}' plugin '#{pluginname}' for application '#{appdir}'"
+        if (!File.directory?("#{appdir}"))
+                raise Thor::Error, "No application '#{appdir}' has been found"
+                return
+            end
+        if (!File.directory?($application_path + "/templates/plugins/opencart/#{plugintype}"))
+                raise Thor::Error, "No templates found for plugin type '#{plugintype}'"
+                return
+            end
+        directory $application_path + "/templates/plugins/opencart/#{plugintype}/", "#{appdir}/", opts
+    end
+    
     no_tasks do
         def pluginname
             @pluginname.downcase
@@ -40,31 +68,18 @@ module Meg
         def language
             $language
         end
+        
+        def extension
+            $extension
+        end
+        
+        def extension_class
+            $extension.capitalize
+        end
     
         def nowformated
             Time.now.strftime('%Y%m%d%H%M%S')
         end
-    end
-
-    method_option :plugintype, :type => :string, :aliases => "-t", :default => "admin-extension"
-    method_option :appdir, :type => :string, :aliases => "-a", :required => true
-    desc "plugin PLUGINNAME", "Generates a new plugin named PLUGINNAME"
-    def plugin(pluginname, *fields)
-	self.pluginname = pluginname
-	plugintype = options[:plugintype]
-	appdir = options[:appdir]
-	opts = { :pluginname => pluginname, :fields => fields }
-        
-    puts "Generating '#{plugintype}' plugin '#{pluginname}' for application '#{appdir}'"
-	if (!File.directory?("#{appdir}"))
-            raise Thor::Error, "No application '#{appdir}' has been found"
-            return
-        end
-	if (!File.directory?($application_path + "/templates/plugins/opencart/#{plugintype}"))
-            raise Thor::Error, "No templates found for plugin type '#{plugintype}'"
-            return
-        end
-	directory $application_path + "/templates/plugins/opencart/#{plugintype}/", "#{appdir}/", opts
     end
 
   end
